03b80ffe7f8e67e9fd0612f3a4994ad5712088c6
Martin Maidana <martinnicolasmaidana@gmail.com>
2022-08-24 09:58:50 -0300

    Proyecto inicial Js

---
+++ script.js
@@ -0,0 +1,91 @@
+
+let cantProductos = 0;
+// Array
+let producto = [0, 0, 0, 0]
+let esparragos = producto[0]
+let brocoli =  producto[1]
+let espinacas = producto[2]
+let zanahorias = producto[3]
+let totalproductos = 0
+
+
+
+//while . ciclo para ir sumando productos. que se repite mientras el usuario ingrese si y se corta cuando el usuario ingrese no/ESC
+while (cantProductos >= 0) {
+    if (prompt('Desea agregar productos si/no/ESC') == 'si'){
+          producto = prompt("VENTA DE PRODUCTOS CONGELADOS: \n Ofrecemos: \n \t 1-ESPARRAGOS, \n \t 2-BROCOLI, \n \t 3-ESPINACAS, \n \t 4-ZANAHORIAS. \n Ingrese el NUMERO del producto que desea. Para finalizar, escriba ESC.");
+
+        if (producto == 1){
+            esparragos++
+
+            cantProductos++
+        };
+        if (producto == 2){
+            brocoli++
+
+            cantProductos++
+
+        };
+        if (producto == 3){
+            espinacas++
+
+            cantProductos++
+
+        };
+        if (producto == 4){
+            zanahorias++
+
+            cantProductos++
+
+        };
+
+        if (producto == 'ESC'){
+            break;
+        }
+    } else {
+        break;
+    }
+}
+//Muestra la cantidad de productos asquiridos en la consola
+console.log('Has comprado: ' + esparragos + ' cantidad de esparragos.')
+console.log('Has comprado: ' + brocoli + ' cantidad de brocolis.')
+console.log('Has comprado: ' + espinacas + ' cantidad de espinacas.')
+console.log('Has comprado: ' + zanahorias + ' cantidad de zanahorias.');
+console.log('En total has adquirido ' + cantProductos + ' cantidad de productos.')
+
+totalproductos = ((esparragos * 700)+ (brocoli * 800) + (espinacas * 900) + (zanahorias * 1000));
+
+//Funcion para calcular el pago en cuotas o en efectivo
+function medios_de_pago(){
+    let pago;
+
+        if (cantProductos > 0){
+        pago = prompt("desea abonar en efectivo (10% DE DESCUENTO) o cuotas 3/6/9/12?");
+        if (pago == "efectivo"){
+        return "el total a abonar EN EFECTIVO, con el 10% de descuento realizado es: " + (totalproductos*0.9 );
+        }
+        if(pago == 3){
+            let pago_en_3_cuotas = totalproductos * 0.15;
+            return "El precio final son 3 cuotas de $ " + pago_en_3_cuotas + " .Disfrute su compra."
+        }
+        if(pago == 6){
+            let pago_en_6_cuotas = totalproductos * 0.20;
+            return "El precio final son 6 cuotas de $ " + pago_en_6_cuotas + " .Disfrute su compra."
+        }
+        if(pago == 9){
+            let pago_en_9_cuotas = totalproductos * 0.25;
+            return "El precio final son 9 cuotas de $  " + pago_en_9_cuotas + " .Disfrute su compra."
+        }
+        if(pago == 12){
+            let pago_en_12_cuotas = totalproductos * 0.30;
+            return "El precio final son 12 cuotas de $  " + pago_en_12_cuotas + " .Disfrute su compra."
+        }
+        else{
+            return "Debe ingresar una opcion validad (efectivo o numero de cuotas 3/6/9/12)"
+        }
+}
+}
+console.log(medios_de_pago());
+
+
+ 
