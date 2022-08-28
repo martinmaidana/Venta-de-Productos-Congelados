d8fabeb6119a1ad813e26ed27bc6c155156d8978
Martin Maidana <martinnicolasmaidana@gmail.com>
2022-08-28 20:50:39 -0300

    primera entraga del proyecto final

--- script.js
+++ script.js
@@ -1,101 +1,120 @@
+//cree un array de objetos. la clase Producto tiene las variables que voy a utilizar. nombre es reconocido como string y precio y stock son reconocidos como numeros. 
 
+class Producto {
+    constructor(nombre, precio, stock) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+        this.stock = parseInt(stock);
+    }
+}
+// //Declaramos un array de productos para almacenar objetos. Es un arreglo que esta vacio
+const productosTotales = [
+    { nombre: "esparragos", precio: 700, stock: 4, carro: 0 },
+    { nombre: "brocoli", precio: 800, stock: 3, carro: 0 },
+    { nombre: "espinacas", precio: 900, stock: 2, carro: 0 },
+    { nombre: "zanahorias", precio: 1000, stock: 0, carro: 0 },
+]
+
+const productos = productosTotales.filter((producto) => producto.stock > 0);
+
+let i = 1;
+console.log("<--------Bienvenidos a venta de productos congelados------->");
+console.log("<--------------->");
+console.log("<--------------->");
+console.log("<--------------->");
+console.log("<--------------->");
+console.log("<--------------->");
+
+console.log("Lista de productos");
+for (let listado of productos) {
+    console.log( i + '-> ' + listado.nombre + ' a $ '+ listado.precio);
+    i++;
+    // console.log("Precio producto ", listado.precio);
+}
+
+// console.log("bienvenidos a venta de productos congelados")
 let cantProductos = 0;
-// Array
-let producto = [0, 0, 0, 0]
-let esparragos = producto[0]
-let brocoli =  producto[1]
-let espinacas = producto[2]
-let zanahorias = producto[3]
-let totalproductos = 0
-
-
 
 //while . ciclo para ir sumando productos. que se repite mientras el usuario ingrese si y se corta cuando el usuario ingrese no/ESC
 while (cantProductos >= 0) {
-    if (prompt('Desea agregar productos \nsi \nno \nESC') == 'si'){
-          producto = prompt("VENTA DE PRODUCTOS CONGELADOS:\n Ingrese el NUMERO del producto que desea. \n Ofrecemos: \n \t 1-ESPARRAGOS, \n \t 2-BROCOLI, \n \t 3-ESPINACAS, \n \t 4-ZANAHORIAS. \n Ingrese el NUMERO del producto que desea. Para finalizar, escriba ESC.");
+    let agregar = prompt("Desea agregar productos si/no/ESC").toUpperCase();
 
-        if (producto == 1){
-            esparragos++
+    if (agregar === "NO") {
+        break
+    } else if (agregar === "SI") {
+        let mensaje = parseInt(prompt("VENTA DE PRODUCTOS CONGELADOS:\n Puede ver la lista de nuestros productos en la consola. \n Ingrese el NUMERO del producto que desea. Para finalizar, escriba ESC."));
 
+
+        if (mensaje === 1) {
+            productos[0].carro = productos[0].carro + 1;
             cantProductos++
-        };
-        if (producto == 2){
-            brocoli++
-
-            cantProductos++
-
-        };
-        if (producto == 3){
-            espinacas++
-
+        } else if (mensaje === 2) {
+            productos[1].carro = productos[1].carro + 1;
             cantProductos++
 
-        };
-        if (producto == 4){
-            zanahorias++
+        } else if (mensaje === 3) {
+            productos[2].carro = productos[2].carro + 1;
+            cantProductos++
 
+        } else if (mensaje === 4) {
+            productos[3].carro = productos[3].carro + 1;
             cantProductos++
 
         };
 
-        if (producto == 'ESC'){
-            break;
-        }
+
     } else {
-        break;
+        console.log("vamos a un corte")
+        break
     }
 }
-//Muestra la cantidad de productos asquiridos en la consola
-if(esparragos !=0){
-    console.log('Has comprado: ' + esparragos + ' cantidad de esparragos.');
-}
-if(brocoli !=0){
-    console.log('Has comprado: ' + brocoli + ' cantidad de brocolis.');
-}
-if(espinacas !=0){
-    console.log('Has comprado: ' + espinacas + ' cantidad de espinacas.')
-}
-if(zanahorias != 0){
-    console.log('Has comprado: ' + zanahorias + ' cantidad de zanahorias.');
+
+// evalua si se compraron productos. si es diferente a 0 muestra la cantidad de productos comprados
+for (let listado of productos) {
+    if (listado.carro > 0){
+        console.log('Has comprado '+ listado.carro + ' cantidad de ' + listado.nombre)
+    }
+    // console.log("Precio producto ", listado.precio);
 }
 
+//Muestra EL TOTAL de productos asquiridos en la consola
+console.log('En total has adquirido ' + cantProductos + ' productos.')
 
-console.log('En total has adquirido ' + cantProductos + ' cantidad de productos.')
 
-totalproductos = ((esparragos * 700)+ (brocoli * 800) + (espinacas * 900) + (zanahorias * 1000));
+const productosAdquiridos = productos.filter((producto) => producto.carro > 0);
+let costoTotal = 0;
+function operaciones() {
+    return costoTotal = productosAdquiridos.reduce((acumulador, elemento) => acumulador + elemento.precio, 0)
+}
+console.log('El costo total es $ ' + operaciones());
+
+// console.log('Hola')
+
 
 //Funcion para calcular el pago en cuotas o en efectivo
-function medios_de_pago(efectivo, cuotas){
-    let pago;
 
-        if (cantProductos > 0){
-        pago = prompt("Ingrese 1. para abonar en EFECTIVO. \nIngrese 2. para abonar en CUOTAS");
-        if (pago == 1){
-        return "El total a abonar EN EFECTIVO, con el 10% de descuento realizado es: " + (totalproductos*0.9);
-        }
-        if (pago == 2) {
-            pago = prompt("Elija la cantidad de cuotas 3 /6 / 9 / 12.")
-            if(pago == 3){
-            let pago_en_3_cuotas = totalproductos * 0.15;
-            return "El precio final son 3 cuotas de $ " + pago_en_3_cuotas + " .Disfrute su compra."
-            }
-            if(pago == 6){
-            let pago_en_6_cuotas = totalproductos * 0.20;
-            return "El precio final son 6 cuotas de $ " + pago_en_6_cuotas + " .Disfrute su compra."
-            }
-            if(pago == 9){
-            let pago_en_9_cuotas = totalproductos * 0.25;
-            return "El precio final son 9 cuotas de $  " + pago_en_9_cuotas + " .Disfrute su compra."
-            }
-            if(pago == 12){
-            let pago_en_12_cuotas = totalproductos * 0.30;
-            return "El precio final son 12 cuotas de $  " + pago_en_12_cuotas + " .Disfrute su compra."
-             }
-        else{
-            return "Debe ingresar una opcion validad (efectivo o numero de cuotas 3/6/9/12)"
+function medios_de_pago() {
+    let tipo = parseInt(prompt("Ingrese 1. para abonar en EFECTIVO. \nIngrese 2. para abonar en CUOTAS"));
+    if (tipo === 1) {
+       console.log("El total a abonar EN EFECTIVO, con el 10% de descuento realizado es $: " + (operaciones() * 0.9));
+    } else if (tipo === 2) {
+        let pago = parseInt(prompt("Elija la cantidad de cuotas 3 /6 / 9 / 12."));
+        if (pago === 3) {
+            let pago_en_3_cuotas = (operaciones() * 1.15)/3;
+            console.log("El precio final es $ " + operaciones() + " en 3 cuotas de $ " + pago_en_3_cuotas + " .Disfrute su compra.")
+        } else if (pago === 6) {
+            let pago_en_6_cuotas = (operaciones() * 1.20)/6;
+            console.log("El precio final es $ " + operaciones() + " en 6 cuotas de $ " + pago_en_6_cuotas + " .Disfrute su compra.")
+        } else if (pago === 9) {
+            let pago_en_9_cuotas = (operaciones() * 1.25)/9;
+            console.log("El precio final es $ " + operaciones() + " en 9 cuotas de $ " + pago_en_9_cuotas + " .Disfrute su compra.")
+        } else if (pago === 12) {
+            let pago_en_12_cuotas = (operaciones() * 1.30)/12;
+            console.log("El precio final es $ " + operaciones() + " en 12 cuotas de $ " + pago_en_12_cuotas + " .Disfrute su compra.")
+        } else {
+            console.log("Debe ingresar una opcion valida (Efectivo o numero de cuotas 3/6/9/12)");
+            medios_de_pago();
         }
     }
 }
-}
-console.log(medios_de_pago());
+medios_de_pago();
