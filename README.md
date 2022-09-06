f21933b2f12a53d555f64dc12f111ee2290caff8
Martin Maidana <martinnicolasmaidana@gmail.com>
2022-09-05 22:54:12 -0300

    eventos

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # 41575-Maidana-Martin- 
 <h1>PRIMER PROYECTO DE JAVASCRIPT</h1>
 <h2>VENTA DE PRODUCTOS CONGELADOS</h2>
+
+
+EVENTOS
+
+linea 21 a 27 evento mouseover. mouseout
+
+linea 38 aplique keydown. en realidad queria hacer que al apretar la tecla enter saliera el mismo mensaje que al apretar el boton aceptar. pero no me salio. 
+
+linea 148 evento click 
+
+linea 155 evento mousedown y mouseup.
+
+linea 168 evento submit.
--- script1.js
+++ script1.js
@@ -1,11 +1,12 @@
-                                //EDITANDO DOM DESDE EL NAVEGADOR
-                                //HEADER
+//EDITANDO DOM DESDE EL NAVEGADOR
+//HEADER
 
 //Accediendo al nodo a traves de get element by ID
 let titulo = document.getElementById("titulo");
 console.log(titulo);
 //defini un atributo de clase en el h1. que tiene un background color para poner el fondo celeste
 titulo.className = "fondoceleste";
+
 
 //innerText de un nodo nos permite modificar su nodo de texto "pisando el html" En este caso cambie el texto del H1.
 console.log(titulo.innerText);
@@ -17,8 +18,15 @@ subth3.innerHTML = "<h3 class='colorazul'>Listado de vegetales</h3>";
 
 let subtitulo = document.getElementById("subtitulo");
 console.log(subtitulo);
+//mouseover/mouseout en el subtitulo vegetales. cambiando el color rojo al pasar el maouse y retorna a negro al sacar el maouse
+subtitulo.addEventListener("mouseover", function () {
+    this.style.color = '#ff0000';
+});
+subtitulo.addEventListener("mouseout", function () {
+    this.style.color = '#000000';
+});
 
-                             //MAIN
+//MAIN
 
 //defini una variable con un valor vacio. para usarlo en una funcion
 let valor = ("");
@@ -27,11 +35,15 @@ function saludando() {
     let nombre = document.getElementById("nombre_usuario");
     let ingreso_al_sistma = document.getElementById("ingresar_al_sistema");
     console.log("<--------Bienvenidos a venta de productos congelados------->", nombre.value);
+    nombre.addEventListener("keydown", function (e) {
+        if (e.key == "t") {
+            console.log("escribiste t");
+        }
+    })
 
 
     if (nombre.value != valor) {
         ingreso_al_sistma.innerHTML = "<p><--------Bienvenidos a venta de productos congelados-------></p>" + nombre.value;
-        console.log("agrega los productos que quieras")
     }
     else {
         ingreso_al_sistma.innerHTML = "<p>Para poder operar, tenes que ingresar un nombre</p>";
@@ -39,6 +51,7 @@ function saludando() {
     }
 
 }
+
 
 //cree un array de objetos. la clase Producto tiene las variables que voy a utilizar. nombre es reconocido como string y precio y stock son reconocidos como numeros. 
 
@@ -51,26 +64,26 @@ class Producto {
 }
 // //Declaramos un array de productos para almacenar objetos. 
 const productosTotales = [
-    { nombre: "esparragos", precio: 700, stock: 4, carro: 0, img: 'esparragos.jpg' },
-    { nombre: "brocoli", precio: 800, stock: 8, carro: 0, img:'brocoli.jpg' },
-    { nombre: "espinacas", precio: 900, stock: 2, carro: 0, img:'espinaca.webp' },
-    { nombre: "zanahorias", precio: 1000, stock: 1, carro: 0, img:'zanahoria.jpg' },
-    { nombre: "zapallo", precio: 1200, stock: 6, carro: 0, img:'zapallo.jpg' },
-    { nombre: "repollo", precio: 1100, stock: 0, carro: 0, img:'repollo.webp' },
-]
+    { id: 1, nombre: "Brocoli", precio: 800, stock: 8, carro: 0, img: 'brocoli.jpg' },
+    { id: 2, nombre: "Espinacas", precio: 900, stock: 2, carro: 0, img: 'espinaca.webp' },
+    { id: 3, nombre: "Zanahorias", precio: 1000, stock: 1, carro: 0, img: 'zanahoria.jpg' },
+    { id: 4, nombre: "Zapallo", precio: 1200, stock: 6, carro: 0, img: 'zapallo.jpg' },
+    { id: 5, nombre: "Repollo", precio: 1100, stock: 0, carro: 0, img: 'repollo.webp' },
+    { id: 6, nombre: "Esparragos", precio: 700, stock: 4, carro: 0, img: 'esparragos.jpg' },
+];
 
 const productos = productosTotales.filter((producto) => producto.stock >= 0);
 
 let i = 1;
-// usando query selector .  ingrese las etiquetas div al html y con for of, recorry el array de objetos. con if . else if y else en funcion del stock y usando clases de css diferencie si el stock esta por agotarse o esta agotado.
+// usando query selector .  ingrese las etiquetas div al html y con for of, recorri el array de objetos. con if . else if y else en funcion del stock y usando clases de css diferencie si el stock esta por agotarse o esta agotado.
 let data = '';
 let vstock = '';
 const cards = document.querySelector('.listadovegetales');
 console.log("Lista de productos");
 for (let listado of productos) {
-    if (listado.stock  == '1' || listado.stock  == '2' || listado.stock  == '3'){
+    if (listado.stock == '1' || listado.stock == '2' || listado.stock == '3') {
         vstock = `<span class="alerta">Stock ${listado.stock} - Quedan Pocas Unidades</span>`;
-    } else if(listado.stock == 0){
+    } else if (listado.stock == 0) {
         vstock = `<span class="agotado">Stock ${listado.stock} - AGOTADO!</span>`;
     } else {
         vstock = 'Stock ' + listado.stock;
@@ -80,21 +93,87 @@ for (let listado of productos) {
     <p>$ ${listado.precio}</p>
     <p>${vstock}</p>
     <label for="number">${listado.nombre}</label>
-    <button type="submit">Añadir</button>
+    <br>
+    <button class="addButton" type="submit" itemID="${listado.id}"">Añadir</button>
 </div>`
-cards.innerHTML = data;
+    cards.innerHTML = data;
     i++
 }
 
-// console.log("bienvenidos a venta de productos congelados")
-let cantProductos = 0;
+//Detectar la funcion Añadir al carrito
+let addButton = document.getElementsByClassName("addButton");
+var myFunction = function () {
+    var attribute = this.getAttribute("itemID");
+    productoSeleccionado = productos.find(obj => obj.id == attribute);
+    productoSeleccionado.carro++
+    updateCarrito()
+};
+
+for (var j = 0; j < addButton.length; j++) {
+    addButton[j].addEventListener('click', myFunction, false);
+}
+
+
+function updateCarrito() {
+    let i = 1;
+    // usando query selector .  ingrese las etiquetas div al html y con for of, recorri el array de objetos. con if . else if y else en funcion del stock y usando clases de css diferencie si el stock esta por agotarse o esta agotado.
+    let data = '';
+    const cards = document.querySelector('.carro');
+    for (let listado of productos) {
+        data += `<article>
+        <div class="imgContainer">
+            <img src="./assets/${listado.img}" alt="${listado.name}" />
+        </div>
+        <p>Cantidad: ${listado.carro}</p>
+    </article>`
+        cards.innerHTML = data;
+        i++
+    }
+    calcularPreciototal()
+}
+
+function calcularPreciototal() {
+    const productosAdquiridos = productos.filter((producto) => producto.carro > 0);
+    let costoTotal = 0;
+    function operaciones() {
+        costoTotal = productosAdquiridos.reduce((acumulador, elemento) => acumulador + (elemento.precio * elemento.carro), 0)
+        document.getElementById("costoTotal").value = costoTotal;
+        console.log(costoTotal);
+    }
+    operaciones();
+}
 
 
 
+// evento click al apretar boton izq del mouse. tirando un mensaje de alerta, dentro de una funcion anonima
+let btn_continuar = document.getElementById("btn_continuarCompra");
+btn_continuar.addEventListener("click", function () {
+    alert("este boton , mas adelante, servira para volver a añadir cosas al carrito cuando estemos en un paso previo a finalizar la compra")
+})
 
 
+//evento mousedown al presionar. mouseup al levantar el boton y ademas cambia al color amarillo el propio boton
+let btn_finalizar = document.getElementById("btn_finalizarCompra");
+btn_finalizar.addEventListener("mousedown", function presion1() {
+    console.log("pronto con este boton direccionaremos a un espacio donde puedas elegir los medios de pago");
+    document.getElementById("btn_finalizarCompra").style.backgroundColor = '#ffff00';
 
-                                //FOOTER
+});
+//evento mouseup. al levantar la presion del btn izquierdo cambia a color rojo.
+btn_finalizar.addEventListener("mouseup", function presion2() {
+    document.getElementById("btn_finalizarCompra").style.backgroundColor = '#ff0000';
+})
+
+
+let form = document.getElementById("form");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let coment = document.getElementById("comentario");
+        console.log(" El comentario es :", comentario.value);
+
+})
+
+//FOOTER
 
 
 //use createElement para insertar un h4 y le agregue una clase, que determinaba un color al h4
@@ -109,9 +188,3 @@ console.log(li);
 for (let elemento of li) {
     console.log(elemento);
 }
-
-
-
-
-
-
